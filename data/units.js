cabb56addb5de1b0f54d1787e57828a3549a5613
jakjeprog-spec <jakjeprog@gmail.com>
2026-01-18 14:21:32 -0600

    Create units.js

---
+++ data/units.js
@@ -0,0 +1,22 @@
+const Units = [
+	{
+		Name: "Fredbear",
+		Type: "Line",
+		Rarity: "Nightmare",
+		Image: "https://tr.rbxcdn.com/96513741244575/420/420/Image/Png",
+		PlacePrice: 4500,
+		Max: 1,
+		Damage: 1500,
+		Cooldown: 5,
+		Range: 17,
+		Passive: "Opening Act",
+		PassiveDescription: "At the start of each wave, gains +50% damage for 10 seconds. Duration stacks if waves occur rapidly.",
+		Upgrades: [
+			{ Price: 0, Damage: 1500, Cooldown: 5, Range: 17 },
+			{ Price: 3000, Damage: 1750, Cooldown: 4.8, Range: 18 },
+			{ Price: 3500, Damage: 2000, Cooldown: 4.7, Range: 18 },
+			{ Price: 5000, Damage: 2250, Cooldown: 4.6, Range: 19 },
+			{ Price: 7500, Damage: 2500, Cooldown: 4.5, Range: 19 }
+		]
+	}
+];
